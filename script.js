78666ac50059d189d4dd8421bf33cebd0acddf12
KreskowMaster <krystian.gontarek@gmail.com>
2014-03-01 16:47:08 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var f1 = document.getElementById("f1".value);
+var f2 = document.getElementById("f2".value);
+var but = document.getElementById("sub");
+
+but.onclick = document.write(f1 + " " + f2);
